2ff986ceb4028a3727504b50569f1577213b84f0
Alex Karlsen <alexkarlsen7@gmail.com>
2018-11-01 08:59:17 +0100

    Removed file

--- JAI/JAI/src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBi08sG07_qo-UjaXvBRSzLeyBaGNosRVo",
-    authDomain: "jai-handball.firebaseapp.com",
-    databaseURL: "https://jai-handball.firebaseio.com/",
-    projectId: "jai-handball",
-    storageBucket: "jai-handball.appspot.com",
-    messagingSenderId: "652379343440"
-  };
