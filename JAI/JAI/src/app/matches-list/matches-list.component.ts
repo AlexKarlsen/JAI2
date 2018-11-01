969c5125024b629d9315c44cb3f696817d5ee8f8
Alex Karlsen <alexkarlsen7@gmail.com>
2018-11-01 15:07:19 +0100

    Fixed layout and client-side filtering and sorting of matches

--- JAI/JAI/src/app/matches-list/matches-list.component.ts
+++ JAI/JAI/src/app/matches-list/matches-list.component.ts
@@ -11,24 +11,29 @@ export class MatchesListComponent implements OnInit {
   constructor(private _fl: FlamelinkService) { }
   matches;
   ngOnInit() {
-    let date = new Date();
-    this._fl.getApp().content.subscribe('matches', { orderByChild: 'id', /* startAt: date, */ limitToLast: 100},(error, data) => {
+    this._fl.getApp().content.subscribe('matches', { orderByChild: 'id', limitToFirst: 100},(error, data) => {
       if (error) {
         console.error(error);
       }
 
       this.matches = Object.keys(data).map(key => data[ key ]);
-
+      
+      // Today object
+      let date = new Date();
+      // Client-side filtering waiting for flamelink update
+      // Iterate to fid expired events
       this.matches.forEach(i => {
-        if(new Date(i.date).valueOf() < date.valueOf()){
-          console.log(this.matches);
-          this.matches.pop(i);
+        // If expired remove element
+        if(new Date(i.date) < date){
+          // Splice removes at index, count
+          this.matches.splice(i,1);
         }
       });
       // Client-side sorting waiting for flamelink update
       this.matches.sort(function(a,b){
         // Turn your strings into dates, and then subtract them
         // to get a value that is either negative, positive, or zero.
+        // a - b to sort ascending
         return new Date(a.date).getTime() - new Date(b.date).getTime();
       });
       console.log(this.matches)
--- JAI/JAI/src/app/matches/matches.component.ts
+++ JAI/JAI/src/app/matches/matches.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FlamelinkService } from '../flamelink.service';
-import { getLocaleDateTimeFormat } from '@angular/common';
 
 @Component({
   selector: 'app-matches',
@@ -9,19 +8,36 @@ import { getLocaleDateTimeFormat } from '@angular/common';
 })
 export class MatchesComponent implements OnInit {
 
-  matches;
+  nextMatch;
   constructor(private _fl: FlamelinkService) { }
 
   ngOnInit() {
-    let date = new Date().getMilliseconds();
-    this._fl.getApp().content.subscribe('matches', { orderByChild: 'id', startAt: date, limitToLast: 1},(error, data) => {
+    this._fl.getApp().content.subscribe('matches', { orderByChild: 'id', /* startAt: date, limitToLast: 1 */},(error, data) => {
       if (error) {
         console.error(error);
       }
 
-      this.matches = Object.keys(data).map(key => data[ key ]);
+      let matches = Object.keys(data).map(key => data[ key ]);
 
-      console.log(this.matches)
+      // Today object
+      let date = new Date();
+      // Client-side filtering waiting for flamelink update
+      // Iterate to fid expired events
+      matches.forEach(i => {
+        // If expired remove element
+        if(new Date(i.date) < date){
+          // Splice removes at index, count
+          matches.splice(i,1);
+        }
+      });
+      // Client-side sorting waiting for flamelink update
+      matches.sort(function(a,b){
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        // a - b to sort ascending
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
+      this.nextMatch = matches[0];
     });
   }
 
